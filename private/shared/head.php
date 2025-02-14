a58f5fe52e72cd89dd0719d50cb92feef374386d
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-14 16:08:12 -0500

    Create head & general public header

---
+++ private/shared/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php isset($pageTitle) ? $pageTitle : 'Culinnari'; ?></title>
+</head>
+<?php require_once('../private/initialize.php'); ?>
