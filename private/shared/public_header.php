0cd1ba1f47ff8e49b02a16ee804c82a5cecfabd9
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-14 16:08:12 -0500

    Create head & general public header

---
+++ private/shared/public_header.php
@@ -0,0 +1,17 @@
+<?php include_once('head.php') ?>
+
+<header role="banner">
+    <h1>Culinnari: Recipe Sharing</h1>
+    <a href="#"><img src="#" alt=" " title=" "></a>
+    <nav role="navigation">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Recipes</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+    <div id="loginSignup">
+        <img src="#">
+        <a href="#">Login / Signup</a>
+    </div>
+</header>
