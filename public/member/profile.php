46aa18f19973a3d42ee746227a357247dc09869b
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-14 19:03:33 -0500

    Create member folder

---
+++ public/member/profile.php
@@ -0,0 +1,8 @@
+<main id="userProfile" role="main" tabindex="-1">
+    <h2>My Culinnari Profile</h2>
+    <h3>Hello, <?php echo $session->username; ?></h3>
+
+    <h3>My Recipes</h3>
+
+    <h3>My Cookbook</h3>
+</main>
