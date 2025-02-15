200e7475c0b70631e0cc2cdfb53677d338d5a609
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-14 19:00:28 -0500

    Created shared header / nav files

--- private/shared/head.php
+++ private/shared/head.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php isset($pageTitle) ? $pageTitle : 'Culinnari'; ?></title>
+    <link href="../public/css/culinnari.css" rel="stylesheet">
+    <script src="../public/js/script.js" defer></script>
 </head>
-<?php require_once('../private/initialize.php'); ?>
