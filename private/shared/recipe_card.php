a4159329e81a5a76cc9ca1585581e90c47e5d456
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-14 19:03:06 -0500

    Begin creating recipe_card layout

---
+++ private/shared/recipe_card.php
@@ -0,0 +1,13 @@
+<div class="recipeCard">
+    <img src="#">
+    <h3>Recipe Name</h3>
+    <div class="recipeDietTypes">
+        <img src="#">
+    </div>
+    <div class="recipeCardUserRating">
+        <a href="#">UserName</a>
+        <div class="rating-stars" data-rating="0">
+            ★★★★★
+        </div>
+    </div>
+</div>
