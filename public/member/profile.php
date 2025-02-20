419331c7149756c8985fc76e095e716bccbc618f
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-20 12:25:52 -0500

    create layout for profile

--- public/member/profile.php
+++ public/member/profile.php
@@ -1,8 +1,23 @@
+<?php require_once('../../private/initialize.php'); ?>
+<?php $pageTitle = "My Profile | Culinnari"; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
 <main id="userProfile" role="main" tabindex="-1">
     <h2>My Culinnari Profile</h2>
-    <h3>Hello, <?php echo $session->username; ?></h3>
+    <h3>Hello, Username!</h3>
 
-    <h3>My Recipes</h3>
+    <section>
+        <div class="profileSectionHead">
+            <h3>My Recipes</h3>
+            <a href="create_recipe.php" class="secondaryButton">Create recipe</a>
+        </div>
+        <div class="profileCards">
+        
+    </section>
 
-    <h3>My Cookbook</h3>
+    <section>
+        <div class="profileSectionHead">
+            <h3>My Cookbook</h3>
+            <a href="create_cookbook.php"  class="secondaryButton">Create cookbook</a>
+    </section>
 </main>
