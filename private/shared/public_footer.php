1e8c91d42351e91a88ef03571ae136c92cb4a8ef
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-20 12:20:09 -0500

    Create public footer

---
+++ private/shared/public_footer.php
@@ -0,0 +1,22 @@
+<footer role="contentinfo">
+    <div id="logoCR">
+        <img src="images/logo.svg" alt="Culinnnari Logo" title="Culinnari | Share Recipes, Enjoy Food" width="251" height="70">
+        <p>&copy; 2025 Sydney Farrington<p>
+    </div>    
+    <div id="secondaryNav">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="recipe.php">Recipes</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+    </div>
+    <div id="footerUserNav">
+        <ul>
+            <li><a href="#">Log In / Sign Up</a></li>
+        </ul>        
+    </div>
+    <div id="businessInfo">
+        <li><a href="privacyPolicy.php">Privacy Policy</a></li>
+        <a href="mailto:hello@culinnari.com">hello@culinnari.com</a>
+    </div>
+</footer>
