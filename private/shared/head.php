7d614ae95e23e77210dd9ca821a3edc6745dfc55
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-20 12:22:53 -0500

    Change path to main css file

--- private/shared/head.php
+++ private/shared/head.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php isset($pageTitle) ? $pageTitle : 'Culinnari'; ?></title>
-    <link href="../public/css/culinnari.css" rel="stylesheet">
+    <link href="../public/css/main.css" rel="stylesheet">
     <script src="../public/js/script.js" defer></script>
 </head>
