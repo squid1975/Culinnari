187d76cbb057b443e3eb5790e55c2f7b13818bec
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-20 12:23:32 -0500

    Add search function to header

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -1,18 +1,25 @@
-<?php include_once('head.php');?>
-
+<?php include('head.php');?>
 
 <header role="banner">
-    <h1>Culinnari: Recipe Sharing</h1>
-    <a href="#"><img src="#" alt=" " title=" "></a>
+    <h1 class="visuallyHidden">Culinnari | Recipe Sharing</h1>
+    <a href="index.php"><img src="../public/images/logo.svg" width="215" height="60" alt="Culinnari Logo" title=""></a>
     <nav role="navigation">
         <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Recipes</a></li>
-            <li><a href="#">About</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="recipes.php">Recipes</a></li>
+            <li><a href="about.php">About</a></li>
         </ul>
     </nav>
+    <div id="headerSearch">
+        <form class="navSearch" action="searchRecipes.php" method="GET">
+            <label for="navRecipeSearch" class="visuallyHidden">Search:</label>
+            <div>
+                <input type="text" id="recipeSearch" name="recipeQuery" placeholder="Search recipes..">
+                <button type="submit" value="Search"><img src="../public/images/icon/search.svg" width="20" height="20"></button>
+            </div>
+    </div>
     <div id="loginSignup">
-        <img src="#">
-        <a href="#">Login / Signup</a>
+        <img src="../public/images/icon/loginSignupIcon.svg" width="30" height="30" alt="Login or Signup Icon" title="Login or Signup with Culinnari">
+        <a href="../public/login_signup.php">Login / Signup</a>
     </div>
 </header>
