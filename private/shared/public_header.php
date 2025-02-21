db2f4ac1529f453a8e22220f45f56bb7ba773a5e
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-21 15:51:46 -0500

    Change to refactor

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -2,24 +2,26 @@
 
 <header role="banner">
     <h1 class="visuallyHidden">Culinnari | Recipe Sharing</h1>
-    <a href="index.php"><img src="../public/images/logo.svg" width="215" height="60" alt="Culinnari Logo" title=""></a>
+    <a href="<?php echo url_for('/index.php'); ?>">
+        <img src="<?php echo url_for('/images/logo.svg'); ?>" width="215" height="60" alt="Culinnari Logo" title="">
+    </a>
     <nav role="navigation">
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="recipes.php">Recipes</a></li>
-            <li><a href="about.php">About</a></li>
+            <li><a href="<?php echo url_for('index.php'); ?>">Home</a></li>
+            <li><a href="<?php echo url_for('recipes.php'); ?>">Recipes</a></li>
+            <li><a href="<?php echo url_for('about.php'); ?>">About</a></li>
         </ul>
     </nav>
-    <div id="headerSearch">
-        <form class="navSearch" action="searchRecipes.php" method="GET">
-            <label for="navRecipeSearch" class="visuallyHidden">Search:</label>
-            <div>
-                <input type="text" id="recipeSearch" name="recipeQuery" placeholder="Search recipes..">
-                <button type="submit" value="Search"><img src="../public/images/icon/search.svg" width="20" height="20"></button>
-            </div>
-    </div>
-    <div id="loginSignup">
-        <img src="../public/images/icon/loginSignupIcon.svg" width="30" height="30" alt="Login or Signup Icon" title="Login or Signup with Culinnari">
-        <a href="../public/login_signup.php">Login / Signup</a>
-    </div>
+    <?php if(!$session->is_logged_in()){ ?>
+        <div id="headerLoginSignup">
+            <img src=<?php echo url_for('images/icon/loginSignupIcon.svg'); ?> width="30" height="30" alt="Login or Signup Icon" title="Login or Signup with Culinnari">
+            <a href=<?php echo url_for('/login_signup.php'); ?> >Login / Signup</a>
+        </div>
+    <?php } elseif($session->is_logged_in()){ ?>
+        <div id="userHeader">
+            <img src=<?php echo url_for('/images/icon/memberIcon.svg'); ?> width="30" height="30" alt="User Icon" title="User Profile">
+            <a href=<?php echo url_for("/member/profile.php"); ?> ><?php echo $session->username; ?></a>
+            <a href=<?php echo url_for("public/logout.php"); ?> >Logout</a>
+        </div>
+    <?php } ?>
 </header>
--- public/index.php
+++ public/index.php
@@ -5,167 +5,85 @@
 
 <body>
     <main role="main" tabindex="-1">
-        <section id="mainHero">
-            <h2>Cook, Share, Enjoy</h2>
-            <p>With simple, easy-to-read recipes, our goal  is to make cooking focused on food again. No fluff, just feasts.</p>
-        </section>
-
-        <section class="search">
-            <h2>Search</h2>
-            <form action="search_recipes.php" method="GET">
-                <label for="search">Search:</label>
-                <input type="text" id="search" name="recipeQuery" placeholder="Search.." required>
-
-                <label for="mealType">Meal Type</label>
-                <div class="dropdown">
-                    <button type="button" class="dropdown-toggle">Any</button>
-                    <div class="dropdown-menu">
-                        <label><input type="checkbox" name="mealType[]" value="appetizer"> Appetizer</label>
-                        <label><input type="checkbox" name="mealType[]" value="breakfast"> Breakfast</label>
-                        <label><input type="checkbox" name="mealType[]" value="brunch"> Brunch</label>
-                        <label><input type="checkbox" name="mealType[]" value="dessert"> Dessert</label>
-                        <label><input type="checkbox" name="mealType[]" value="dinner"> Dinner</label>
-                        <label><input type="checkbox" name="mealType[]" value="lunch"> Lunch</label>
-                        <label><input type="checkbox" name="mealType[]" value="snack"> Snack</label>
-                    </div>
+        <div id="homeHero">
+            <section id="mainHero">
+                <div id="heroText">
+                    <h2>Cook. Share. Enjoy.</h2>
+                    <p>With simple, easy-to-read recipes, our goal  is to make cooking focused on food again. No fluff, just feasts.</p>
                 </div>
-
-                <label for="style">Style</label>
-                <div class="dropdown">
-                    <button type="button" class="dropdown-toggle">Any</button>
-                    <div class="dropdown-menu">
-                        <label><input type="checkbox" name="style[]" value="asian"> Asian</label>
-                        <label><input type="checkbox" name="style[]" value="caribbean"> Caribbean</label>
-                        <label><input type="checkbox" name="style[]" value="fusion"> Fusion</label>
-                        <label><input type="checkbox" name="style[]" value="italian"> Italian</label>
-                        <label><input type="checkbox" name="style[]" value="latinAmerican"> Latin American</label>
-                        <label><input type="checkbox" name="style[]" value="mediterranean"> Mediterranean</label>
-                        <label><input type="checkbox" name="style[]" value="thai"> Thai</label>
-                    </div>
-                </div>
-
-                <label for="diet">Diet</label>
-                <div class="dropdown">
-                    <button type="button" class="dropdown-toggle">Any</button>
-                    <div class="dropdown-menu">
-                        <label><input type="checkbox" name="diet[]" value="glutenFree"> Gluten-Free</label>
-                        <label><input type="checkbox" name="diet[]" value="keto"> Keto</label>
-                        <label><input type="checkbox" name="diet[]" value="low-carb"> Low Carb</label>
-                        <label><input type="checkbox" name="diet[]" value="paleo"> Paleo</label>
-                        <label><input type="checkbox" name="diet[]" value="sugarFree"> Sugar-Free</label>
-                        <label><input type="checkbox" name="diet[]" value="vegan"> Vegan</label>
-                        <label><input type="checkbox" name="diet[]" value="vegetarian"> Vegetarian</label>
-                    </div>
-                </div>
-
-                <label for="rating">Rating</label>
-                <select id="rating" name="rating">
-                    <option value="anyRating" selected>Any</option>
-                    <option value="5stars">5 stars</option>
-                    <option value="4stars">4 stars & up</option>
-                    <option value="3stars">3 stars & up</option>
-                    <option value="2stars">2 stars & up</option>
-                    <option value="1star">1 star & up</option>
-                </select>
-
-                <label for="video">Video</label>
-                <select id="video" name="video">
-                    <option value="anyVideo" selected>Any</option>
-                    <option value="hasVideo">Has YouTube Video</option>
-                </select>
-
-                <label for="prepCookTimeTotal">Total Time</label>
-                <select id="prepCookTimeTotal" name="prepCookTimeTotal">
-                    <option value="anyPrepCookTimeTotal" selected>Any</option>
-                    <option value="max15Mins">15 minutes or less</option>
-                    <option value="max30Mins">30 minutes or less</option>
-                    <option value="max45mins">45 minutes or less</option>
-                    <option value="oneTwoHours">1-2 hours</option>
-                    <option value="moreThanTwoHours">More than 2 Hours</option>
-                </select>
-
-                <label for="sortBy">Sort By</label>
-                <select id="sortBy" name="sortBy">
-                    <option value="anySortBy" selected>Any</option>
-                    <option value="date_desc">Newest</option>
-                    <option value="date_asc">Oldest</option>
-                    <option value="name_asc">Name Ascending (A-Z)</option>
-                    <option value="name_desc">Name Descending (Z-A)</option>
-                    <option value="rating_desc">Highest Rated</option>
-                    <option value="rating_asc">Lowest Rated</option>
-                </select>
-
-                <button type="submit">Search</button>
-            </form>
-        </section>
-
-        <section id="dietKey"
-            <h2>Find what works for your diet.</h2>
-            <div>
-                <img src="#">
-                <p>Gluten-Free</p>
-            </div>
-            <div>
-                <img src="#">
-                <p>Keto</p>
-            </div>
-            <div>
-                <img src="#">
-                <p>Low Carb</p>
-            </div>
-            <div>
-                <img src="#">
-                <p>Sugar-Free</p>
-            </div>
-            <div>
-                <img src="#">
-                <p>Paleo</p>
-            </div>
-            <div>
-                <img src="#">
-                <p>Vegan</p>
-            </div>
-            <div>
-                <img src="#">
-                <p>Vegetarian</p>
-            </div>
-
-        </section>
-
-        <section class="bestRated">
-            <div class="recipeCard">
-                <img src="#">
-                <h3>Recipe Name</h3>
-                <div class="recipeDietTypes">
-                    <img src="#">
-                </div>
-                <div class="recipeCardUserRating">
-                    <a href="#">UserName</a>
-                    <div class="rating-stars" data-rating="0">
-                        ★★★★★
-                    </div>
-                </div>
-            </div>
-
-        </section>
-
-        <section>
-            <h2>Prep right for your next meal.</h2>
-            <a href="#">Breakfast</a>
-            <a href="#">Lunch</a>
-            <a href="#">Dinner</a>
-            <a href="#">Desserts</a>
-        </section>
-        <div>
-            <section>
-                <h2>Become a member today!</h2>
-                <ul>
-                    <li><img src="#"">Post your own recipes</li>
-                    <li>Rate others recipes</li>
-                    <li>Create cookbooks</li>
-                </ul>
             </section>
+        </div>  
+
+        <?php include(SHARED_PATH . '/search.php');?>
+
+        <div id="wrapper">
+            <section>
+                <h2>Find what works for your diet.</h2>
+                <p>All of our recipes feature color-coded diet icons to help you quickly identify meals that fit your lifestyle. Whether you're looking for vegan, gluten-free, keto, or other dietary preferences, our easy-to-read key ensures you can make informed choices at a glance.</p>
+                <div class="dietKey">
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/glutenFree.svg" width="20" height="20" alt="gluten free icon" title="Gluten free recipe icon">
+                        <p>Gluten-Free</p>
+                    </div>
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/keto.svg" width="20" height="20" alt="keto icon" title="Keto recipe icon">
+                        <p>Keto</p>
+                    </div>
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/lowCarb.svg" width="20" height="20" alt="Low carb icon" title="Low carb recipe icon">
+                        <p>Low Carb</p>
+                    </div>
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/sugarFree.svg" width="20" height="20" alt="Sugar free icon" title="Sugar free recipe icon">
+                        <p>Sugar-Free</p>
+                    </div>
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/paleo.svg" width="20" height="20" alt="Paleo icon" title="Paleo recipe icon">
+                        <p>Paleo</p>
+                    </div>
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/vegan.svg" width="20" height="20"  alt="Vegan icon" title="Vegan recipe icon">
+                        <p>Vegan</p>
+                    </div>
+                    <div class="dietIconWithName">
+                        <img src="images/icon/dietIcons/vegetarian.svg" width="20"  height="20" alt="Vegetarian icon" title="Vegetarian recipe icon">
+                        <p>Vegetarian</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="bestRated">
+                <h2>Rated best by our users</h2>
+                <div class="recipeCardDisplay">
+                    
+                </div>
+
+
+            </section>
+            <div id="suggestionsContent">
+                <section>
+                    <h2>Prep right for your next meal.</h2>
+                    <div id="homeMealTypeSuggestions">
+                        <a href="#" class="homeSectionButton">Breakfast</a>
+                        <a href="#" class="homeSectionButton">Lunch</a>
+                        <a href="#" class="homeSectionButton">Dinner</a>
+                        <a href="#" class="homeSectionButton">Desserts</a>
+                    </div>
+                </section>
+                <div>
+                <section id="ctaPublic">
+                    <h2>Become a member today!</h2>
+                        <ul>
+                            <li><img src="images/icon/pencil.svg" width="20" height="20" alt="A pencil icon" title="Pencil for writing your own recipes">Post your own recipes</li>
+                            <li><img src="images/icon/star.svg" width="20" height="20" alt="A star icon" title="Star rating for recipes">Rate others recipes</li>
+                            <li><img src="images/icon/notebook.svg" width="20" height="20" alt="A cookbook notebook icon" title="Cookbook notebook for saving recipes">Create cookbooks</li>
+                        </ul>
+                        <a href="login_signup.php" class="primaryButton">SIGN UP FOR FREE!</a>
+                </section>
+            </div>
+            </div>
         </div>
     </main>
+    <?php include(SHARED_PATH . '/public_footer.php'); ?>
 </body>
 </html>
