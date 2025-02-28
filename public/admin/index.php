962c0df5833f928db1f4d2d3ce12f2cb68dad9f5
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-28 16:03:44 -0500

    create admin management page

---
+++ public/admin/index.php
@@ -0,0 +1,18 @@
+<?php require_once('../../private/initialize.php'); ?>
+<?php $pageTitle = "Management Area | Culinnari"; ?>
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<main role="main" tabindex="-1">
+    <div id="adminHero">
+        <h2>Management Area</h2>
+    </div>
+    <section>
+        <h3>Categories</h3>
+        <a href="<?php echo url_for('/admin/manage_categories.php'); ?>" class="secondaryButton">Manage Categories</a>
+    </section>
+    <section>
+        <h3>Users</h3>
+        <a href="<?php echo url_for('/admin/manage_users.php'); ?>" class="secondaryButton">Manage Users</a>
+    </section>
+</main>
+    <?php include(SHARED_PATH . '/public_footer.php'); ?>
