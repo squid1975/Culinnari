c91ae76590cf267bfa885238aa900b5c5fa56dd1
Sydney <sydneyrpenix@students.abtech.edu>
2025-02-28 16:03:25 -0500

    change for css formatting

--- private/shared/recipe_card.php
+++ private/shared/recipe_card.php
@@ -1,13 +1,14 @@
 <div class="recipeCard">
-    <img src="#">
-    <h3>Recipe Name</h3>
-    <div class="recipeDietTypes">
-        <img src="#">
-    </div>
-    <div class="recipeCardUserRating">
-        <a href="#">UserName</a>
-        <div class="rating-stars" data-rating="0">
-            ★★★★★
+    <a href="<?php echo url_for('/recipe.php'); ?>">
+        <img class="recipeCardImage" src="<?php echo url_for('/images/uploads/recipe_image/food_placeholder.jpg');?>" width="250" height="250" alt="recipe" title="recipe ">
+        <p>This is the name of the recipe</p>
+        <div class="recipeCardDietIcons">
+            <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg');?>" width="20" height="20" alt="gluten free icon" title="Gluten free recipe icon">
+            <img src="<?php echo url_for('/images/icon/dietIcons/keto.svg');?>" width="20" height="20" alt="keto icon" title="Keto recipe icon">
+            <img src="<?php echo url_for('/images/icon/dietIcons/lowCarb.svg');?>" width="20" height="20" alt="Low carb icon" title="Low carb recipe icon">
         </div>
-    </div>
+        <div class="rating">
+            <img src="<?php echo url_for('/images/icon/star.svg');?>" width="20" height="20" alt="star icon" title="star icon">
+        </div>
+    </a>
 </div>
