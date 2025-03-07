56841c7533c0162acc88840da4b3d1fb52183c94
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-07 18:23:41 -0500

    make dynamic data for page

--- public/view_recipe.php
+++ public/view_recipe.php
@@ -1,61 +1,92 @@
 <?php require_once('../private/initialize.php'); 
-$pageTitle = "Recipe:  | Culinnari"; 
-include(SHARED_PATH . '/public_header.php'); ?>
+$id = $_GET['recipe_id'] ?? '1';
+$recipe = Recipe::find_by_id($id);
+$pageTitle = "Recipe: " . h($recipe->recipe_name) . " | Culinnari"; 
+
+$ingredients = Ingredient::find_by_recipe_id(($id));
+$steps = Step::find_by_recipe_id(($id));
+
+$video = RecipeVideo::find_by_recipe_id($id);
+include(SHARED_PATH . '/public_header.php'); 
+
+
+?>
+
 
 <main role="main" tabindex="-1">
+    <div id="recipePageDisplayWrapper">
+        <div id="recipeNameImageDesc">
+            
+            <?php if (!empty($images)): ?>
+                <?php foreach ($images as $image): ?>
+                    <img src="<?php echo $image->recipe_image_url; ?>" width="300" height="300" 
+                        alt="<?php echo 'Photo for ' . h($recipe->recipe_name); ?>">
+                <?php endforeach; ?>
+            <?php else: ?>
+                <img src="<?php echo url_for('/images/default_recipe_image.webp'); ?>" width="300" height="300" 
+                    alt="Default recipe image">
+            <?php endif; ?>
+            
+            <h2><?php echo h($recipe->recipe_name); ?></h2>
+            <p id="recipeDisplayrecipeDescription"><?php echo h($recipe->recipe_description); ?></p>
+            
+            <div id="iconsStars">
+                <div id="recipeDisplayDietIcons">
+                    <?php if(!empty($diets)): ?>
+                        <?php foreach ($diets as $diet): ?>
+                            <img src="<?php echo u($diet->diet_icon_url); ?>" width="20" height="20" alt="<?php echo h($diet->diet_name); ?>">
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
+                
+                <div id="recipeDisplayRatingStars">
+                <svg width="20" height="20" viewBox="0 0 24 24" stroke="black" fill="yellow"><polygon points="12,2 15,9 22,9 17,14 18,21 12,17 6,21 7,14 2,9 9,9" /></svg>
+                <svg width="20" height="20" viewBox="0 0 24 24" stroke="black" fill="yellow"><polygon points="12,2 15,9 22,9 17,14 18,21 12,17 6,21 7,14 2,9 9,9" /></svg>
+                <svg width="20" height="20" viewBox="0 0 24 24" stroke="black" fill="yellow"><polygon points="12,2 15,9 22,9 17,14 18,21 12,17 6,21 7,14 2,9 9,9" /></svg>
+                <svg width="20" height="20" viewBox="0 0 24 24" stroke="black" fill="yellow"><polygon points="12,2 15,9 22,9 17,14 18,21 12,17 6,21 7,14 2,9 9,9" /></svg>
+                <svg width="20" height="20" viewBox="0 0 24 24" stroke="black" fill="yellow"><polygon points="12,2 15,9 22,9 17,14 18,21 12,17 6,21 7,14 2,9 9,9" /></svg>
+                </div>
+                
+                <div id="recipeDisplayDifficulty">
+                    <?php if($recipe->recipe_difficulty === 'beginner'){ ?>
+                        <p id="recipeDisplayBeginner"><?php echo h($recipe->recipe_difficulty); ?></p>
+                    <?php } elseif($recipe->recipe_difficulty === 'intermediate'){ ?>
+                        <p id="recipeDisplayIntermediate"><?php echo h($recipe->recipe_difficulty); ?></p>
+                    <?php } else{ ?>
+                        <p id="recipeDisplayAdvanced"><?php echo h($recipe->recipe_difficulty); ?></p>
+                    <?php } ; ?>    
+                        
+                </div>
+            </div>
+            
+            <div id="recipeDisplayprepCook">
+                <?php
+                // Convert prep time from seconds to hours and minutes
+                $prep_hours = floor($recipe->recipe_prep_time_seconds / 3600); // Get whole hours
+                $prep_minutes = floor(($recipe->recipe_prep_time_seconds % 3600) / 60); // Get remaining minutes
 
-<div id="recipePageDisplayWrapper">
-    <div id="recipeNameImageDesc">
-        <img src="<?php echo url_for('/images/default_recipe_image.webp'); ?>" width="300" height="300">
-        <h2>Gluten Free Oreo No Bake Cheesecake</h2>
-        <p id="recipeDisplayrecipeDescription">This gluten-free no-bake Oreo cheesecake features a crunchy Oreo crust, a creamy, smooth cheesecake filling, and a topping of crushed Oreos. Quick to make and perfect for any occasion, it's a delicious treat for those avoiding gluten!</p>
-        <div id="iconsStars">
-            <div id="recipeDisplayDietIcons">
-                <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg'); ?>">
-            </div>
-            <div id="recipeDisplayRatingStars">
-                <img src="<?php echo url_for('/images/icon/star.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/star.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/star.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/star.svg'); ?>">
-                <img src="<?php echo url_for('/images/icon/star.svg'); ?>">
-            </div>
-            <div id="recipeDisplayDifficulty">
-                <p>Beginner</p>
+                // Convert cook time from seconds to hours and minutes
+                $cook_hours = floor($recipe->recipe_cook_time_seconds / 3600); // Get whole hours
+                $cook_minutes = floor(($recipe->recipe_cook_time_seconds % 3600) / 60); // Get remaining minutes
+                ?>
+
+                <?php if ($prep_hours > 0 || $prep_minutes > 0): ?>
+                    <p>Prep Time: <?php echo h($prep_hours) . " hours " . h($prep_minutes) . " minutes"; ?></p>
+                <?php endif; ?>
+
+                <?php if ($cook_hours > 0 || $cook_minutes > 0): ?>
+                    <p>Cook Time: <?php echo h($cook_hours) . " hours " . h($cook_minutes) . " minutes"; ?></p>
+                <?php endif; ?>
             </div>
         </div>
-        <div id="recipeDisplayprepCook">
-            <p>Prep Time: 15 Minutes</p>
-            <p>Cook Time: 3 Hours</p>
-        </div>
-    </div>
-    
-        <div id="recipeDisplayOptions">
-            <a href="about:blank" onclick="window.print(); return false;">
-                <img src="<?php echo url_for('/images/icon/print.svg'); ?>" width="24" height="24" alt="Printer icon" title="Print recipe">
-                Print Recipe
-            </a>
-            <a href="#" ">
-                <img src="<?php echo url_for('/images/icon/addToCookbook.svg'); ?>" width="24" height="24" alt="Printer icon" title="Print recipe">
-                Add to Cookbook
-            </a>
-            <a href="#" ">
-                <img src="<?php echo url_for('/images/icon/star.svg'); ?>" width="24" height="24" alt="Printer icon" title="Print recipe">
-                Add Rating
-            </a>
-        </div>
+        
+        
 
         <div id="recipeDisplayIngredientsSteps">
-
             <div id="recipeDisplayIngredients">
                 <h3>Ingredients</h3>
                 <div id="recipeDisplayTotalServings">
-                    <h4>Total Servings: <span id="servingAmt">8</span></h4>
+                    <h4>Total Servings: <span id="servingAmt"><?php echo h($recipe->recipe_total_servings); ?></span></h4>
                     <div id="recipeDisplayChangeServingAmt">
                         <button id="halfButton">1/2</button>
                         <button id="1time">1x</button>
@@ -63,48 +94,75 @@ include(SHARED_PATH . '/public_header.php'); ?>
                         <button id="3time">3x</button>
                     </div>
                 </div>
-                <ul>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                    <li class="recipeIngredientListing"><span id="measurementAmount">20</span> Eggs</li>
-                </ul>
+                <div id="recipeDisplayIngredientList">
+                    <ul>
+                        <?php if(!empty($ingredients)): ?>
+                            <?php foreach ($ingredients as $ingredient): ?>
+                                <li class="recipeIngredientListing">
+                                    <span id="recipeDisplayMeasurementAmount"><?php echo h(decimal_to_fraction($ingredient->ingredient_quantity)); ?></span> 
+                                    <?php if (!empty($ingredient->ingredient_measurement_name) && $ingredient->ingredient_measurement_name !== 'n/a'): ?>
+                                        <span><?php echo h($ingredient->ingredient_measurement_name); ?></span>
+                                    <?php endif; ?>
+                                    <?php echo h($ingredient->ingredient_name); ?>
+                                </li>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </ul>
+                </div>
             </div>
 
             <div id="recipeDisplaySteps">
                 <h3>Steps</h3>
                 <ol>
-                  <li> Prepare the crust: Crush the gluten-free Oreo cookies into fine crumbs using a food processor or by placing them in a sealed bag and crushing them with a rolling pin.</li>
-                  <li>Mix the crumbs with the melted butter until combined.</li>
-                  <li>Press the mixture into the bottom of a pie dish or spring-form pan to form a firm crust.</li>
-                  <li>Place the crust in the fridge for 30 minutes to chill while making the filling. </li>
-                  <li>In a large mixing bowl, combine the softened cream cheese with powdered sugar and vanilla extract until smooth and creamy. </li>
-                  <li>In a separate bowl, whip the heavy cream until stiff peaks form.</li>
-                  <li>Gently fold the whipped cream into the cream cheese mixture until fully combined. </li>
-                  <li>Slowly stir in crushed gluten-free Oreo’s for added texture and flavor.</li>
-                  <li>Remove the chilled pie crust from the fridge.</li>
-                  <li>Pour the cheesecake filling over the chilled crust and smooth the top with a spatula. </li>
-                  <li>Sprinkle crushed Oreo’s on top to garnish. </li>
-                  <li>Refrigerate the cheesecake for at least 3 hours or more to set.</li>
-                  <li>After the cheesecake is set, slice and serve chilled. </li>
-                </ol>
+                    <?php if(!empty($steps)): ?>
+                        <?php foreach ($steps as $step): ?>
+                            <li><?php echo h($step->step_description); ?></li>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </ol>        
             </div>
-
         </div>
 
+        <?php if(!empty($video)): ?>
         <div id="recipeDisplayYouTubeVideo">
             <h3>Watch YouTube Video</h3>
             <iframe width="420" height="315"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                src="https://www.youtube.com/embed/<?php echo h($video->video_url); ?>">
             </iframe>
         </div>
+        <?php endif; ?>
 
+        <div id="recipeDisplayOptions">
+            <div>
+                <a href="about:blank" onclick="window.print(); return false;">
+                    <img src="<?php echo url_for('/images/icon/print.svg'); ?>" width="24" height="24" alt="Printer icon" title="Print recipe">
+                    Print Recipe
+                </a>
+            </div>
+            <div>
+                <label for="cookbook_checkbox" class="button">
+                    <img src="<?php echo url_for('/images/icon/addToCookbook.svg'); ?>" width="24" height="24" alt="Cookbook icon" title="Add to cookbook">
+                    Add to Cookbook
+                </label>
+                    <input type="checkbox" id="cookbook_checkbox" class="cookbook_checkbox">
+                    <div id="cookbook_form">
+                        <!-- Form for selecting cookbook -->
+                        <form class="cookbook_form" action="addrecipetocookbook.php" method="POST">
+                        <h4>Add this recipe to your cookbook?</h4>
+                        <!-- Hidden input fields inside the form to submit recipe data -->
+                        <input type="hidden" name=recipe[id] value="$recipe->id;">
+
+                        <button type="submit" class="submit_button">Add Recipe</button>
+                        <button type="button" class="cancel_button">Cancel</button>
+                    </form>
+                    </div>
+            </div>
+            <a href="#">
+                <img src="<?php echo url_for('/images/icon/star.svg'); ?>" width="24" height="24" alt="Star icon" title="Add rating">
+                Add Rating
+            </a>
+        </div>
     </div>
-
+</main>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
