230d1e045c5c6dfb0f8b9dfd5c6534953f814ae7
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-07 18:25:22 -0500

    attempt to make diet icons display in recipe card

--- private/shared/recipe_card.php
+++ private/shared/recipe_card.php
@@ -1,15 +1,29 @@
+<?php if (isset($recipe)): ?>
+    <div class="recipeCard">
+        <a href="<?php echo url_for('/view_recipe.php?recipe_id=' . $recipe->id); ?>">
+        <?php 
+        $recipeImage = RecipeImage::find_image_by_recipe_id($recipe->id); 
+        if ($recipeImage):  // Ensure an image exists before displaying
+        ?>
+            <img class="recipeCardImage" src="<?php echo url_for($recipeImage->recipe_image); ?>" width="250" height="250" alt="recipe" title="recipe">
+        <?php endif; ?>
 
-<div class="recipeCard">
-    <a href="<?php echo url_for('/recipe.php'); ?>">
-        <img class="recipeCardImage" src="<?php echo url_for('/images/uploads/recipe_image/food_placeholder.jpg');?>" width="250" height="250" alt="recipe" title="recipe ">
-        <p>This is the name of the recipe</p>
+        <p><?php echo h($recipe->recipe_name); ?></p>
+
         <div class="recipeCardDietIcons">
-            <img src="<?php echo url_for('/images/icon/dietIcons/glutenFree.svg');?>" width="20" height="20" alt="gluten free icon" title="Gluten free recipe icon">
-            <img src="<?php echo url_for('/images/icon/dietIcons/keto.svg');?>" width="20" height="20" alt="keto icon" title="Keto recipe icon">
-            <img src="<?php echo url_for('/images/icon/dietIcons/lowCarb.svg');?>" width="20" height="20" alt="Low carb icon" title="Low carb recipe icon">
+            <?php 
+            $diets = $recipe->getDiets($recipe->id);
+            if (!empty($diets)): 
+            ?>
+                <?php foreach ($diets as $diet): ?>
+                    <img src="<?php echo u($diet->diet_icon_url); ?>" alt="Diet Icon">
+                <?php endforeach; ?>
+            <?php endif; ?>  
         </div>
+
         <div class="rating">
-            <img src="<?php echo url_for('/images/icon/star.svg');?>" width="20" height="20" alt="star icon" title="star icon">
+            <img src="<?php echo url_for('/images/icon/star.svg'); ?>" width="20" height="20" alt="star icon" title="star icon">
         </div>
     </a>
-</div>
+    </div>
+<?php endif; ?>
