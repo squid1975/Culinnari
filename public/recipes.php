ac67aa227ee59a04067d5f0238d240c896a7c11d
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-07 18:26:14 -0500

    query brainstorming

--- public/recipes.php
+++ public/recipes.php
@@ -1,13 +1,31 @@
-<?php require_once('../private/initialize.php'); ?>
-<?php $pageTitle = "Recipes | Culinnari"; ?>
-<?php include(SHARED_PATH . '/public_header.php'); ?>
+<?php require_once('../private/initialize.php'); 
+ $pageTitle = "Recipes | Culinnari"; 
+ include(SHARED_PATH . '/public_header.php'); 
 
+
+ $filters=[];
+
+ // Get all the values from the form (via the GET method)
+ $filters = [
+    'keyword' => $_GET['recipeQuery'] ?? null,
+    'mealType' => $_GET['mealType'] ?? null,
+    'style' => $_GET['style'] ?? null,
+    'diet' => $_GET['diet'] ?? null,
+    'difficulty' => $_GET['difficulty'] ?? null,
+    'video' => $_GET['video'] ?? null,
+    'prepCookTimeTotal' => $_GET['prepCookTimeTotal'] ?? null,
+    'sortBy' => $_GET['sortBy'] ?? null
+];
+
+
+?>
 
 <main role="main" tabindex="-1">
         <section id="searchHero">
             <div id="heroText">
                 <h2>Find your next feast.</h2>
             </div> 
+            <?php include SHARED_PATH .'/recipe_search.php' ?>
         <div class="dietKey">
                     <div class="dietIconWithName">
                         <img src="images/icon/dietIcons/glutenFree.svg" width="20" height="20" alt="gluten free icon" title="Gluten free recipe icon">
@@ -38,11 +56,26 @@
                         <p>Vegetarian</p>
                     </div>
                 </div>
-    <?php include(SHARED_PATH . '/search.php');?>
+                
 
+    <?php if (!empty($results)): ?>
     <div id="results">
-        
+        <h3>Results</h3>
+        <?php foreach ($results as $result): ?>
+            <?php include(SHARED_PATH . '/recipe_card.php'); ?>
+        <?php endforeach; ?>
     </div>
+<?php else: ?>
+    <?php $allRecipes = DatabaseObject::find_by_sql("SELECT * FROM recipe"); ?>
+    <div id="allRecipes">
+        <h3>All Recipes</h3>
+       
+            <?php foreach ($allrecipes as $allrecipe): ?>
+                <?php include(SHARED_PATH . '/recipe_card.php'); ?>
+            <?php endforeach; ?>
+       
+    </div>
+<?php endif; ?>
 </main>
 
 <?php include(SHARED_PATH . '/public_footer.php'); ?>
