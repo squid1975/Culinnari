5b68db295e89de3b6e77b9bef8ec5031d885e436
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-11 01:22:34 -0400

    fix login/signup link

--- private/shared/admin_header.php
+++ private/shared/admin_header.php
@@ -1,20 +1,23 @@
-<?php require_login(); ?> 
+<?php require_mgmt_login(); ?>
 <div id="adminNav">
-    <div>
-    <a href="<?php echo url_for('/member/profile.php'); ?>" >
-        <img src="<?php echo url_for('/images/icon/adminIcon.svg');
-             ?>" width="30" height="30" alt="User Icon" title="User Profile">
-             <?php echo h($session->username); ?> (Admin)</a>
-    </div>
-    <div>         
-        <a href="<?php echo url_for('/member/create_recipe.php'); ?>">
-            <img src="<?php echo url_for('/images/icon/addRecipe.svg'); ?>" width="20" height="20" alt="Add recipe icon" title="Add recipe">
-            Post a recipe</a>     
+    <input type="checkbox" id="adminMenuToggle">
+    <label for="adminMenuToggle">
+            <img src="<?php echo url_for('/images/icon/adminIcon.svg'); ?>" width="30" height="30" alt="User Icon" title="User Profile">
+            <?php echo h($session->username); ?>
+    </label>
+
+    <div id="adminMenu">         
+        <a href="<?php echo url_for('/member/profile.php'); ?>">
+            <img src="<?php echo url_for('/images/icon/profileIcon.svg'); ?>" width="20" height="20" alt="Profile Icon" title="View culinnari profile">
+            My Profile
+        </a>     
         <a href="<?php echo url_for('/admin/index.php'); ?>">
             <img src="<?php echo url_for('/images/icon/settings.svg'); ?>" width="20" height="20" alt="Admin Icon" title="Admin Management">
-            Management</a>
-        <a href="<?php echo url_for('/logout.php'); ?> ">
+            Management
+        </a>
+        <a href="<?php echo url_for('/logout.php'); ?>">
             <img src="<?php echo url_for('/images/icon/logout.svg'); ?>" width="20" height="20" alt="Logout Icon" title="Logout">
-            Logout</a>
+            Logout
+        </a>
     </div>
 </div>
