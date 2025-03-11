823db3f309fe37700edc791a1cc7b4dacb188427
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-11 01:23:02 -0400

    Fix login link

--- private/shared/public_footer.php
+++ private/shared/public_footer.php
@@ -13,12 +13,10 @@
     <div id="footerUserNav">
         <ul>
             <?php if(!$session->is_logged_in()){ ?>
-            <li><a href="<?php echo url_for('/login_signup.php');?>">Log In</a></li>
-            <li><a href="<?php echo url_for('/login_signup.php');?>">Create Account</a></li>
+            <li><a href="<?php echo url_for('/login_signup.php');?>">Log In / Create Account</a></li>
             <?php } elseif($session->is_logged_in()) { ?>
             <li><a href="<?php echo url_for('/member/profile.php'); ?>">My Profile</a></li>
             <li><a href="<?php echo url_for('/member/create_recipe.php'); ?> ">Create Recipe</a></li>
-            <li><a href="<?php echo url_for('/member/cookbook.php'); ?>">My Cookbook</a></li>
             <?php } ?>
             <?php if($session->is_mgmt_logged_in()){ ?>
             <li><a href="<?php echo url_for('/admin/index.php'); ?>">Management Area</a></li>
@@ -26,8 +24,7 @@
         </ul>        
     </div>
     <div id="businessInfo">
-        <li><a href="privacyPolicy.php">Privacy Policy</a></li>
-        <li><a href="mailto:hello@culinnari.com">hello@culinnari.com</a></li>
+        <li><img src="<?php echo url_for('/images/icon/email.svg'); ?>" width="30" height="30" alt="Email icon" title="Email culinnari"><a href="mailto:hello@culinnari.com">hello@culinnari.com</a></li>
     </div>
 </footer>
 </html>
