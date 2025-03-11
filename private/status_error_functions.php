fdb7d8918b64c11c7db5755c918caf33178dde17
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-11 01:22:55 -0400

    fixed error function

--- private/status_error_functions.php
+++ private/status_error_functions.php
@@ -28,7 +28,7 @@ function display_errors($errors=array()) {
   $output = '';
   if(!empty($errors)) {
     $output .= "<div class=\"errors\">";
-    $output .= "Please fix the following errors:";
+    $output .= "Error:";
     $output .= "<ul>";
     foreach($errors as $error) {
       $output .= "<li>" . h($error) . "</li>";
