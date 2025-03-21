d04223d846d408fa3e93e6e529ae0521b774f2d3
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-21 13:12:54 -0400

    Added stylesheet for rating functionality

--- private/shared/head.php
+++ private/shared/head.php
@@ -8,5 +8,6 @@ $id = $_SESSION['user_id'] ?? false; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php isset($pageTitle) ? $pageTitle : 'Culinnari'; ?></title>
     <link href="<?php echo url_for('css/global.css'); ?>" rel="stylesheet">
-    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
 </head>
