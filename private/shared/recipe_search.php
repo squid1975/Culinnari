dff3450f29bc0221c997b057b2e332ca8e58e8d7
Sydney <sydneyrpenix@students.abtech.edu>
2025-03-21 13:18:44 -0400

    Change prep cook time total and category id values to ints

--- private/shared/recipe_search.php
+++ private/shared/recipe_search.php
@@ -3,14 +3,14 @@ $mealTypes = MealType::find_all();
 $styles = Style::find_all();
 $diets = Diet::find_all(); 
 ?>
-
+<script src="<?php echo url_for('js/script.js'); ?>" defer></script>
 <div id="searchWithFilters">
     <h2 class="visuallyHidden">Search</h2>
-        <form action=<?php echo url_for('/recipes.php'); ?> method="GET" id="searchForm">
+        <form action="<?php echo url_for('/recipes.php'); ?>" method="GET" id="searchForm">
             <div id="searchBox">
                 <div>
                     <label for="mainSearch" class="visuallyHidden">Search</label>
-                    <input type="search" id="mainSearch" name="recipeQuery" placeholder="SEARCH..">
+                    <input type="search" id="mainSearch" name="recipeQuery" pattern="[A-Za-z\s]+" placeholder="SEARCH..">
                 </div>
                 <button type="submit" class="mainSearchButton" aria-label="Search">
                     <img src="images/icon/search.svg" alt="search icon">
@@ -28,7 +28,7 @@ $diets = Diet::find_all();
                                 <div class="dropdown-content">
                                     <?php foreach ($mealTypes as $mealType): ?>
                                         <label>
-                                            <input type="checkbox" name="meal_types[]" id="mealType" value="<?php echo $mealType->id; ?>">
+                                            <input type="checkbox" name="mealTypes[]" id="mealType" value=<?php echo $mealType->id; ?>>
                                             <?php echo ucfirst($mealType->meal_type_name); ?>
                                         </label>
                                     <?php endforeach; ?>
@@ -44,7 +44,7 @@ $diets = Diet::find_all();
                                 <div class="dropdown-content">
                                 <?php foreach ($styles as $style): ?>
                                         <label>
-                                            <input type="checkbox" name="styles[]" id="style" value="<?php echo $style->id; ?>">
+                                            <input type="checkbox" name="styles[]" id="style" value=<?php echo $style->id; ?>>
                                             <?php echo ucfirst($style->style_name); ?>
                                         </label>
                                     <?php endforeach; ?>
@@ -60,7 +60,7 @@ $diets = Diet::find_all();
                                 <div class="dropdown-content">
                                     <?php foreach ($diets as $diet): ?>
                                         <label>
-                                            <input type="checkbox" name="diets[]" id="diet" value="<?php echo $diet->id; ?>">
+                                            <input type="checkbox" name="diets[]" id="diet" value=<?php echo $diet->id; ?>>
                                             <?php echo ucfirst($diet->diet_name); ?>
                                         </label>
                                     <?php endforeach; ?>
@@ -89,11 +89,11 @@ $diets = Diet::find_all();
                             <div class="dropdown">
                                 <button type="button" class="dropdown-button">ANY</button> <!-- Button says ANY -->
                                 <div class="dropdown-content">
-                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value="max15Mins"> 15 mins or less</label>
-                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value="max30Mins"> 30 mins or less</label>
-                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value="max45mins"> 45 mins or less</label>
-                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value="oneTwoHours"> 1-2 hours</label>
-                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value="moreThanTwoHours">2+ hours</label>
+                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value=900> 15 mins or less</label> 
+                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value=1800> 30 mins or less</label> 
+                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value=2700> 45 mins or less</label> 
+                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value=3600-7200> 1-2 hours</label>
+                                    <label><input type="checkbox" name="prepCookTimeTotal[]" value=7200+>2+ hours</label> 
                                 </div>
                             </div>
                         </div>
@@ -102,11 +102,11 @@ $diets = Diet::find_all();
                         <div class="searchFilter">
                             <label for="sortBy">Sort By</label>
                             <div class="dropdown">
-                                <button type="button" class="dropdown-button">ANY</button> <!-- Button says ANY -->
+                                <button type="button" class="dropdown-button">Default</button> 
                                 <div class="dropdown-content">
-                                    <label><input type="radio" name="sortBy" value="default">Newest</label>
-                                    <label><input type="radio" name="sortBy" value="newest">Oldest</label>
-                                    <label><input type="radio" name="sortBy" value="oldest">A-Z</label>
+                                    <label><input type="radio" name="sortBy" value="recipe[recipe_post_date] DESC" checked>Newest</label>
+                                    <label><input type="radio" name="sortBy" value="recipe[recipe_post_date] ASC">Oldest</label>
+                                    <label><input type="radio" name="sortBy" value="recipe[recipe_name] ASC">A-Z</label>
                                     <label><input type="radio" name="sortBy" value="ratingdesc">Rating: High to Low</label>
                                     <label><input type="radio" name="sortBy" value="ratingasc">Rating: Low to High</label>
                                 </div>
