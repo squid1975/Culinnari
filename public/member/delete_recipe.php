5ff5b144e2e190e71466a27b59b895b86ea22bef
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-03 13:31:26 -0400

    delete recipe functionality

---
+++ public/member/delete_recipe.php
@@ -0,0 +1,19 @@
+<?php
+require_login();
+require_once('../../private/initialize.php'); 
+if(!isset($_GET['recipe_id'])) {
+    redirect_to(url_for('/index.php'));
+  }
+$id = $_GET['recipe_id'];
+$recipe = Recipe::find_by_id($id);
+if($recipe == false){
+    $_SESSION['message'] = 'Recipe not found.';
+}
+
+if(is_post_request()){
+    $result = $recipe->delete();
+    $_SESSION['message'] = 'The recipe was deleted successfully.';
+    redirect_to(url_for('/member/profile.php?id=' . h($user->id)));
+} else {
+    $_SESSION['message'] = 'Something went wrong. Please try again later.';
+}
