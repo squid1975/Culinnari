87ba435db4720774e7284a7948bc737ba78d2ba3
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-03 13:27:44 -0400

    included favicon

--- private/shared/head.php
+++ private/shared/head.php
@@ -7,6 +7,7 @@ $id = $_SESSION['user_id'] ?? false; ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php isset($pageTitle) ? $pageTitle : 'Culinnari'; ?></title>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link href="<?php echo url_for('css/global.css'); ?>" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="<?php echo url_for('/js/search.js'); ?>" defer></script>
