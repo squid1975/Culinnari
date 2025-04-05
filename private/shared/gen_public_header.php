2a9fadae4ac417a8540b98b0e5967f8d24fad9f0
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-05 14:07:39 -0400

    Changed login/signup

--- private/shared/gen_public_header.php
+++ private/shared/gen_public_header.php
@@ -1,4 +1,4 @@
 <div id="headerLoginSignup">
     <img src="<?php echo url_for('/images/icon/loginSignupIcon.svg'); ?>" width="32" height="32"  alt="User login signup icon">
-    <a href="<?php echo url_for('/login_signup.php'); ?>">Login / Signup</a>
+    <a href="<?php echo url_for('/login_signup.php'); ?>">Login</a>
 </div>
