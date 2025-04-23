0556dce63e92d45942995c3cf1cae0a2757f4639
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-22 23:05:36 -0400

    Added simple about page

---
+++ public/about.php
@@ -0,0 +1,28 @@
+<?php require_once('../private/initialize.php'); 
+$title = 'About Us | Culinnari';
+include SHARED_PATH . '/public_header.php'; 
+?>
+<main role="main" tabindex="-1">
+    <div class="wrapper">
+        <h2>About Culinnari</h2>
+            <section class="aboutSection">
+                <h3>Our Mission</h3>
+                <p>At Culinnari, we believe that cooking should be a joyful and accessible experience for everyone. Our mission is to inspire home cooks of all levels to explore the world of culinary arts, discover new flavors, and create delicious meals with confidence.</p>
+                <p>We stand behind our promise to offer short and easy-to-read recipes that bring the focus back to the food. Finding recipes shouldn't require you to scroll through every little detail of the cooks' life. This is why when you're posting on Culinnari, we limit recipe description and recipe step lengths. By doing this, we hope to encourage you to take on cooking with confidence and trust that you can make something amazing. </p>
+                <p>We invite you to join our community of culinary enthusiasts. Whether you're a seasoned chef or just starting out in the kitchen, Culinnari has something for everyone. Explore our recipes, share your own creations, and connect with fellow food lovers.</p>
+            </section>
+
+            <section class="aboutSection">
+                <h3>Our Values</h3>
+                <p>At Culinnari, we are committed to the following values:</p>
+                <ul>
+                    <li><strong>Inclusivity:</strong> We welcome cooks of all backgrounds and skill levels. Everyone was a beginner at one point.</li>
+                    <li><strong>Simplicity:</strong> Our recipes are designed to be straightforward and easy to follow.</li>
+                    <li><strong>Creativity:</strong> We encourage experimentation and creativity in the kitchen.</li>
+                    <li><strong>Community:</strong> We believe in the power of sharing knowledge and experiences.</li>
+                </ul>
+            </section>
+    </div>
+
+<?php
+include SHARED_PATH . '/public_footer.php'; ?>
