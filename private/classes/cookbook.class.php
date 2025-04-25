1bce9a1e617d317985bbf9fb143ca57e0eb2b0b1
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-24 23:38:14 -0400

    validator

--- private/classes/cookbook.class.php
+++ private/classes/cookbook.class.php
@@ -16,6 +16,20 @@ class Cookbook extends DatabaseObject {
         $this->user_id = $args['user_id'] ?? $_SESSION['user_id'];
     }
 
+    protected function validate() {
+        $this->errors[];
+
+        if(is_blank($this->cookbook_name)) {
+            $this->errors[] = "Cookbook name cannot be blank.";
+        } elseif (!has_length($this->cookbook_name, ['min' => 2, 'max' => 255])) {
+            $this->errors[] = "Cookbook name must be between 2 and 255 characters.";
+        } elseif(!preg_match("^[A-Za-z \-']+$")){
+            $this->errors[] = "Cookbook name can only contain letters, spaces, hyphens, and apostrophes.";
+        }
+
+        return $this->errors; 
+    }
+
     /**
      * Returns all cookbooks for a user
      * @param mixed $user_id the id value of the user (id in user table, user_id in cookbook table)
