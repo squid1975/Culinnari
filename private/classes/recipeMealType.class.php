73adcd0339e8414e16e9c3bd1e27237b221b3a6d
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-27 16:19:14 -0400

    PHP docblock

--- private/classes/recipeMealType.class.php
+++ private/classes/recipeMealType.class.php
@@ -16,6 +16,11 @@ static protected $table_name = 'recipe_meal_type';
         $this->meal_type_id = $args['meal_type_id'] ?? 1;
     }
 
+     /**
+     * Find the meal types associated with a recipe
+     * @param int|string $recipe_id The recipe_id (id in recipe table) of the recipe to look up
+     * @return DatabaseObject[] An array of RecipeMealType objects
+     */
     public static function find_by_recipe_id($recipe_id) {
         $sql = "SELECT * FROM " . static::$table_name . " WHERE recipe_id = '" . self::$database->escape_string($recipe_id) . "'";
         $result_array = static::find_by_sql($sql);
