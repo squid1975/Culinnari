008d443aa0b9c6091f0576bc09d319029820db38
Sydney <sydneyrpenix@students.abtech.edu>
2025-04-29 14:43:41 -0400

    fix pass variable

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USER','root');
-define('DB_PASS', 'password');
-define('DB_NAME', 'your_db_name');
+define('DB_SERVER', '');
+define('DB_USER','');
+define('DB_PASS', '');
+define('DB_NAME', '');
